85b9a04f6b15466739469cfacfcb5d235cadee36
gdegjygr <malcevartem808@gmail.com>
2025-05-16 17:05:21 +0600

    upgraded repo

---
+++ localScript.js
@@ -0,0 +1,14 @@
+// It's little conflict for your repo
+function main() {
+    let OS_b = 0;
+
+    let name = "WIN32"
+    let version = 11
+
+    console.log(name + version);
+    for (let x = 0; x >= 0; x++){
+        console.log(OS_b);
+    }
+}
+
+main()
